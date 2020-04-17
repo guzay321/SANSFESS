fd65bea22639aa10bf0b376d71ecfe7754fd1331
guzay321 <cloneurmind@gmail.com>
2020-04-17 20:21:05 +0700

    dua

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ def start():
                 id = dms[i]['id']
 
                 if len(message) is not 0 and len(message) < 280:
-                    if "ajig" in message:
-                        message = message.replace("ajig", "ajig")
+                    if "ajig" in message or "hehe" in message:
+                        message = message.replace("ajig", "ajig") or message = message.replace("hehe", "hehe")
                         if len(message) is not 0:
                             if dms[i]['media'] is None:
                                 print("DM will be posted")
@@ -40,37 +40,6 @@ def start():
 
             dms = list()
             
-def start():
-    print("Starting program...")
-    dms = list()
-    while True:
-        if len(dms) is not 0:
-            for i in range(len(dms)):
-                message = dms[i]['message']
-                sender_id = dms[i]['sender_id']
-                id = dms[i]['id']
-
-                if len(message) is not 0 and len(message) < 280:
-                    if "hehe" in message:
-                        message = message.replace("hehe", "hehe")
-                        if len(message) is not 0:
-                            if dms[i]['media'] is None:
-                                print("DM will be posted")
-                                tw.post_tweet(message)
-                                tw.delete_dm(id)
-                            else:
-                                print("DM will be posted with media")
-                                print(dms[i]['shorted_media_url'])
-                                tw.post_tweet_with_media(message, dms[i]['media'],dms[i]['shorted_media_url'], dms[i]['type'])
-                                tw.delete_dm(id)
-                        else:
-                            print("DM deleted because its empty..")
-                            tw.delete_dm(id)
-                    else:
-                        print("DM will be deleted because does not contains keyword..")
-                        tw.delete_dm(id)
-
-            dms = list()
 
         else:
             print("Direct message is empty...")
